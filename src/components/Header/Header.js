cec46233027b35c8814f9e21ccef1008c4e2ecf2
kittybagpipes <sparticus978@gmail.com>
2021-01-27 14:36:21 -0800

    styled login and registration page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,57 +1,63 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import TokenService from '../../services/token-service'
-import UserContext from '../../contexts/UserContext'
-import './Header.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import TokenService from "../../services/token-service";
+import UserContext from "../../contexts/UserContext";
+import "./Header.css";
 
 class Header extends Component {
-  static contextType = UserContext
+  static contextType = UserContext;
 
   handleLogoutClick = () => {
-    this.context.processLogout()
-  }
+    this.context.processLogout();
+  };
 
   renderLogoutLink() {
     return (
       <div>
-        <span>
-          {this.context.user.name}
-        </span>
+        <span>{this.context.user.name}</span>
         <nav>
           <Link
+            className="navLink"
             onClick={this.handleLogoutClick}
-            to='/login'>
+            to="/login"
+          >
             Logout
           </Link>
         </nav>
       </div>
-    )
+    );
   }
 
   renderLoginLink() {
     return (
       <nav>
-        <Link to='/login'>Login</Link>
-        {' '}
-        <Link to='/register'>Sign up</Link>
+        <Link className="navLink" to="/login">
+          Login
+        </Link>{" "}
+        <Link className="navLink" to="/register">
+          Sign up
+        </Link>
       </nav>
-    )
+    );
   }
 
   render() {
     return (
       <header>
         <h1>
-          <Link to='/'>
-            Spaced repetition
+          <Link className="headLink" to="/">
+            Langellium
           </Link>
         </h1>
-        {TokenService.hasAuthToken()
-          ? this.renderLogoutLink()
-          : this.renderLoginLink()}
+        <div className="navBar">
+          {TokenService.hasAuthToken()
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()}
+        </div>
+        <hr />
       </header>
     );
   }
 }
 
-export default Header
+export default Header;
--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react'
-import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
-
+import React, { Component } from "react";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+import "./Registration.css";
 class RegistrationRoute extends Component {
   static defaultProps = {
     history: {
       push: () => {},
     },
-  }
+  };
 
   handleRegistrationSuccess = () => {
-    const { history } = this.props
-    history.push('/login')
-  }
+    const { history } = this.props;
+    history.push("/login");
+  };
 
   render() {
     return (
-      <section>
-        <p>
-          Practice learning a language with the spaced reptition revision technique.
-        </p>
-        <h2>Sign up</h2>
-        <RegistrationForm
-          onRegistrationSuccess={this.handleRegistrationSuccess}
-        />
-      </section>
+      <div className="centerDiv">
+        <section className="regSection">
+          <p>
+            Practice learning a language with the spaced reptition revision
+            technique.
+          </p>
+          <h2>Sign up</h2>
+          <RegistrationForm
+            onRegistrationSuccess={this.handleRegistrationSuccess}
+          />
+        </section>
+      </div>
     );
   }
 }
 
-export default RegistrationRoute
+export default RegistrationRoute;
